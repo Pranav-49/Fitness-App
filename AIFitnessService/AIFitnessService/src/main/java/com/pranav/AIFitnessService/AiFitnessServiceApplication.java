fcdceea4d34e8d710b29b98a89e87da524610d22
Pranav-49 <pranavraychure@gmail.com>
2025-09-15 19:12:11 +0530

    Ai recommendation setup

---
+++ AIFitnessService/AIFitnessService/src/main/java/com/pranav/AIFitnessService/AiFitnessServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pranav.AIFitnessService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiFitnessServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiFitnessServiceApplication.class, args);
+	}
+
+}
---
+++ AIFitnessService/AIFitnessService/src/test/java/com/pranav/AIFitnessService/AiFitnessServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pranav.AIFitnessService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiFitnessServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
