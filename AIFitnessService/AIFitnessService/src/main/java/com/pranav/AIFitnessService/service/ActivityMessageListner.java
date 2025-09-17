825d8a688f5e1cbde908a8a82059e37a5f2948f3
Pranav-49 <pranavraychure@gmail.com>
2025-09-17 18:18:12 +0530

    serialization and deserialization done

---
+++ AIFitnessService/AIFitnessService/src/main/java/com/pranav/AIFitnessService/service/ActivityMessageListner.java
@@ -0,0 +1,19 @@
+package com.pranav.AIFitnessService.service;
+
+import com.pranav.AIFitnessService.model.Activity;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ActivityMessageListner {
+
+    @KafkaListener(topics = "activity-events" , groupId = "activity-processor-group")
+    public void processActivity(Activity activity)
+    {
+        log.info("Received Activity for Processing : {}",activity.getUserId());
+    }
+}
